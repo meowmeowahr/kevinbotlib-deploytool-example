ac5d3e15ab59ae49316d03052fca93a5dbb5bb41
meowmeowahr <meowmeowahr@gmail.com>
2025-03-23 17:28:48 -0500

    TRACE logging

--- src/kevinbotlib_deploytool_example/main.py
+++ src/kevinbotlib_deploytool_example/main.py
@@ -1,9 +1,10 @@
+from kevinbotlib.logger import Level
 from kevinbotlib.robot import BaseRobot
 
 
 class ExampleRobot(BaseRobot):
     def __init__(self):
-        super().__init__(["DeployedRobotOpMode"])
+        super().__init__(["DeployedRobotOpMode"], log_level=Level.TRACE)
 
     def opmode_init(self, opmode: str, enabled: bool) -> None:
         super().opmode_init(opmode, enabled)
