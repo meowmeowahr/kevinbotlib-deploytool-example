b28db6fde843793ce82fec37a53798f7dc2945a2
meowmeowahr <meowmeowahr@gmail.com>
2025-03-23 19:56:35 -0500

    Add example usage of deploy and assets directory

--- src/kevinbotlib_deploytool_example/main.py
+++ src/kevinbotlib_deploytool_example/main.py
@@ -9,3 +9,9 @@ class ExampleRobot(BaseRobot):
     def opmode_init(self, opmode: str, enabled: bool) -> None:
         super().opmode_init(opmode, enabled)
         print("Example robot started up...")  # noqa: T201
+
+        with open("deploy/example.txt", "r") as f:
+            print(f.read())
+
+        with open("assets/example.txt", "r") as f:
+            print(f.read())
